581ce453d4e610e23153b36eb662a536c6842e59
Bente <bente.vandepol@gmail.com>
2019-10-15 11:49:34 +0200

    Trying to add lighthouse2 folder

---
+++ lighthouse2/apps/tinyapp/main.cpp
@@ -0,0 +1,137 @@
+/* main.cpp - Copyright 2019 Utrecht University
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#include "platform.h"
+#include "system.h"
+#include "rendersystem.h"
+
+static RenderAPI* renderer = 0;
+static GLTexture* renderTarget = 0;
+static Shader* shader = 0;
+static uint scrwidth = 0, scrheight = 0, car = 0;
+static bool camMoved = false, hasFocus = true, running = true;
+
+#include "main_tools.h"
+
+//  +-----------------------------------------------------------------------------+
+//  |  PrepareScene                                                               |
+//  |  Initialize a scene.                                                  LH2'19|
+//  +-----------------------------------------------------------------------------+
+void PrepareScene()
+{
+	// initialize scene
+	renderer->AddScene( "scene.gltf", "data\\pica\\" );
+	int rootNode = renderer->FindNode( "RootNode (gltf orientation matrix)" );
+	renderer->SetNodeTransform( rootNode, mat4::RotateX( PI / 2 ) );
+	int carID = renderer->AddMesh( "legocar.obj", "data\\", 10.0f );
+	int lightMat = renderer->AddMaterial( make_float3( 100, 100, 80 ) );
+	int lightQuad = renderer->AddQuad( make_float3( 0, -1, 0 ), make_float3( 0, 26.0f, 0 ), 6.9f, 6.9f, lightMat );
+	renderer->AddInstance( lightQuad );
+	car = renderer->AddInstance( carID );
+}
+
+//  +-----------------------------------------------------------------------------+
+//  |  HandleInput                                                                |
+//  |  Process user input.                                                  LH2'19|
+//  +-----------------------------------------------------------------------------+
+bool HandleInput( float frameTime )
+{
+	// handle keyboard input
+	float translateSpeed = (GetAsyncKeyState( VK_SHIFT ) ? 15.0f : 5.0f) * frameTime, rotateSpeed = 2.5f * frameTime;
+	bool changed = false;
+	Camera* camera = renderer->GetCamera();
+	if (GetAsyncKeyState( 'A' )) { changed = true; camera->TranslateRelative( make_float3( -translateSpeed, 0, 0 ) ); }
+	if (GetAsyncKeyState( 'D' )) { changed = true; camera->TranslateRelative( make_float3( translateSpeed, 0, 0 ) ); }
+	if (GetAsyncKeyState( 'W' )) { changed = true; camera->TranslateRelative( make_float3( 0, 0, translateSpeed ) ); }
+	if (GetAsyncKeyState( 'S' )) { changed = true; camera->TranslateRelative( make_float3( 0, 0, -translateSpeed ) ); }
+	if (GetAsyncKeyState( 'R' )) { changed = true; camera->TranslateRelative( make_float3( 0, translateSpeed, 0 ) ); }
+	if (GetAsyncKeyState( 'F' )) { changed = true; camera->TranslateRelative( make_float3( 0, -translateSpeed, 0 ) ); }
+	if (GetAsyncKeyState( 'B' )) changed = true; // force restart
+	if (GetAsyncKeyState( VK_UP )) { changed = true; camera->TranslateTarget( make_float3( 0, -rotateSpeed, 0 ) ); }
+	if (GetAsyncKeyState( VK_DOWN )) { changed = true; camera->TranslateTarget( make_float3( 0, rotateSpeed, 0 ) ); }
+	if (GetAsyncKeyState( VK_LEFT )) { changed = true; camera->TranslateTarget( make_float3( -rotateSpeed, 0, 0 ) ); }
+	if (GetAsyncKeyState( VK_RIGHT )) { changed = true; camera->TranslateTarget( make_float3( rotateSpeed, 0, 0 ) ); }
+	// let the main loop know if the camera should update
+	return changed;
+}
+
+//  +-----------------------------------------------------------------------------+
+//  |  main                                                                       |
+//  |  Application entry point.                                             LH2'19|
+//  +-----------------------------------------------------------------------------+
+int main()
+{
+	// initialize OpenGL
+	InitGLFW();
+
+	// initialize renderer: pick one
+	// renderer = RenderAPI::CreateRenderAPI( "rendercore_optix7.dll" );			// OPTIX7 core, best for RTX devices
+	// renderer = RenderAPI::CreateRenderAPI( "rendercore_vulkan_rt.dll" );			// Meir's Vulkan / RTX core
+	renderer = RenderAPI::CreateRenderAPI( "rendercore_optixprime_b.dll" );			// OPTIX PRIME, best for pre-RTX CUDA devices
+	// renderer = RenderAPI::CreateRenderAPI( "rendercore_primeref.dll" );			// REFERENCE, for image validation
+	// renderer = RenderAPI::CreateRenderAPI( "rendercore_optixrtx_b.dll" );		// OPTIX6 core, for reference
+	// renderer = RenderAPI::CreateRenderAPI( "rendercore_softrasterizer.dll" );	// RASTERIZER, your only option if not on NVidia
+	// renderer = RenderAPI::CreateRenderAPI( "rendercore_optix7filter.dll" );		// OPTIX7 core, with filtering, in progress
+
+	renderer->DeserializeCamera( "camera.xml" );
+	// initialize scene
+	PrepareScene();
+	// set initial window size
+	ReshapeWindowCallback( 0, SCRWIDTH, SCRHEIGHT );
+	// enter main loop
+	Timer timer;
+	timer.reset();
+	float deltaTime = 0;
+	while (!glfwWindowShouldClose( window ))
+	{
+		renderer->SynchronizeSceneData();
+		Convergence c = Converge;
+		if (camMoved) c = Restart, camMoved = false;
+		// detect camera changes
+		if (renderer->GetCamera()->Changed()) camMoved = true;
+		// poll events, may affect probepos so needs to happen between HandleInput and Render
+		glfwPollEvents();
+		// render
+		deltaTime = timer.elapsed();
+		timer.reset();
+		renderer->Render( c );
+		if (HandleInput( deltaTime )) camMoved = true;
+	#if 1
+		// minimal rigid animation example
+		static float r = 0;
+		renderer->SetNodeTransform( car, mat4::RotateY( r * 2.0f ) * mat4::RotateZ( 0.2f * sinf( r * 8.0f ) ) * mat4::Translate( make_float3( 0, 5, 0 ) ) );
+		r += deltaTime * 0.3f; if (r > 2 * PI) r -= 2 * PI;
+		camMoved = true;
+	#endif
+		// finalize and present
+		shader->Bind();
+		shader->SetInputTexture( 0, "color", renderTarget );
+		shader->SetInputMatrix( "view", mat4::Identity() );
+		DrawQuad();
+		shader->Unbind();
+		// finalize
+		glfwSwapBuffers( window );
+		// terminate
+		if (!running) break;
+	}
+	// clean up
+	renderer->SerializeCamera( "camera.xml" );
+	renderer->Shutdown();
+	glfwDestroyWindow( window );
+	glfwTerminate();
+	return 0;
+}
+
+// EOF
---
+++ lighthouse2/clean.bat
@@ -0,0 +1,95 @@
+rem | This file deletes all files that can be regenerated with 
+rem | a rebuild / run in VS2017. Execute this when archiving, 
+rem | handing in assignments etc.
+rem | Make sure VS2017 is *closed* for best results.
+
+del *.suo /Q
+del *.suo /AH /Q
+del *.sdf /Q
+rd ipch /S /Q
+rd .vs /S /Q
+
+rem | Clean up RenderCore folders
+
+rd lib\RenderCore_OptixPrime\lib /S /Q
+rd lib\RenderCore_OptixPrime\x64 /S /Q
+rd lib\RenderCore_PrimeRef\lib /S /Q
+rd lib\RenderCore_PrimeRef\x64 /S /Q
+rd lib\RenderCore_OptixPrime_B\lib /S /Q
+rd lib\RenderCore_OptixPrime_B\x64 /S /Q
+rd lib\RenderCore_OptixRTX\lib /S /Q
+rd lib\RenderCore_OptixRTX\x64 /S /Q
+del lib\RenderCore_OptixRTX\optix\*.ptx /S /Q
+rd lib\RenderCore_OptixRTX_B\lib /S /Q
+rd lib\RenderCore_OptixRTX_B\x64 /S /Q
+del lib\RenderCore_OptixRTX_B\optix\*.ptx /S /Q
+rd lib\RenderCore_Optix7\lib /S /Q
+rd lib\RenderCore_Optix7\x64 /S /Q
+del lib\RenderCore_Optix7\optix\*.ptx /S /Q
+rd lib\RenderCore_Optix7Filter\lib /S /Q
+rd lib\RenderCore_Optix7Filter\x64 /S /Q
+del lib\RenderCore_Optix7Filter\optix\*.ptx /S /Q
+rd lib\RenderCore_SoftRasterizer\lib /S /Q
+rd lib\RenderCore_SoftRasterizer\x64 /S /Q
+rd lib\RenderCore_RTX_AO\lib /S /Q
+rd lib\RenderCore_RTX_AO\x64 /S /Q
+del lib\RenderCore_RTX_AO\optix\*.ptx /S /Q
+rd lib\RenderCore_OpenCL\lib /S /Q
+rd lib\RenderCore_OpenCL\x64 /S /Q
+rd lib\RenderCore_Embree\x64 /S /Q
+rd lib\RenderCore_Embree\lib /S /Q
+rd lib\RenderCore_vulkan_rt\lib /S /Q
+rd lib\RenderCore_vulkan_rt\x64 /S /Q
+rd coredlls\debug /S /Q
+rd coredlls\release /S /Q
+
+rem | Clean up applications
+
+rd apps\basicapp\x64 /S /Q
+del apps\basicapp\data\textures\*.bin
+del apps\basicapp\data\sky_15.bin
+del apps\basicapp\*.exe /Q
+del apps\basicapp\*.iobj /Q
+del apps\basicapp\*.ipdb /Q
+del apps\basicapp\*.pdb /Q
+del apps\basicapp\*.ilk /Q
+rd apps\tinyapp\x64 /S /Q
+del apps\tinyapp\data\textures\*.bin
+del apps\tinyapp\data\sky_15.bin
+del apps\tinyapp\*.exe /Q
+del apps\tinyapp\*.iobj /Q
+del apps\tinyapp\*.ipdb /Q
+del apps\tinyapp\*.pdb /Q
+del apps\tinyapp\*.ilk /Q
+rd apps\app_matui\x64 /S /Q
+del apps\app_matui\data\textures\*.bin
+del apps\app_matui\data\mattest\textures\*.bin
+del apps\app_matui\data\sky_15.bin
+del apps\app_matui\*.exe /Q
+del apps\app_matui\*.iobj /Q
+del apps\app_matui\*.ipdb /Q
+del apps\app_matui\*.pdb /Q
+del apps\app_matui\*.ilk /Q
+rd apps\imguiapp\x64 /S /Q
+del apps\imguiapp\data\textures\*.bin
+del apps\imguiapp\data\mattest\textures\*.bin
+del apps\imguiapp\data\sky_15.bin
+del apps\imguiapp\*.exe /Q
+del apps\imguiapp\*.iobj /Q
+del apps\imguiapp\*.ipdb /Q
+del apps\imguiapp\*.pdb /Q
+del apps\imguiapp\*.ilk /Q
+rd apps\rtxbvhreveng\x64 /S /Q
+del apps\rtxbvhreveng\data\sky_15.bin
+del apps\rtxbvhreveng\*.exe /Q
+del apps\rtxbvhreveng\*.iobj /Q
+del apps\rtxbvhreveng\*.ipdb /Q
+del apps\rtxbvhreveng\*.pdb /Q
+del apps\rtxbvhreveng\*.ilk /Q
+
+rem | Clean up other components
+
+rd lib\RenderSystem\lib /S /Q
+rd lib\RenderSystem\x64 /S /Q
+rd lib\platform\lib /S /Q
+rd lib\platform\x64 /S /Q
