579ed3967b19de3ef90d4da202ee2ee38227c0e3
Bente <bente.vandepol@gmail.com>
2019-10-18 12:49:43 +0200

    Fixed lighthouse project

--- SIMD_Test/SIMD/SIMD.cpp
+++ SIMD_Test/SIMD/SIMD.cpp
@@ -4,7 +4,7 @@
 
 int main()
 {
-	printf("hoi");
+	printf("hoi \n");
 
 	//__m128 x4 = _mm_set_ps(1.0f, 1.0f, 1.0f, 1.0f);
 	//union { __m128 mask1; float mask1f[4]; };
@@ -12,9 +12,9 @@ int main()
 	//std::cout << "Print mask" << mask1[0] << ", " << mask1[1] << ", " << mask1[2] << ", " << mask1[3] << "\n";
 
 	__m128 x4 = _mm_set_ps(1.0f, 1.0f, 1.0f, 1.0f);
-	union { __m128 mask1; float mask1f[4]; };
+	union { __m128 mask1; unsigned int mask1f[4]; };
 	mask1 = _mm_cmpge_ps(x4, _mm_setzero_ps());
-	std::cout << "Print mask" << mask1f[0] << ", " << mask1f[1] << ", " << mask1f[2] << ", " << mask1f[3] << "\n";
+	std::cout << "Print mask " << mask1f[0] << ", " << mask1f[1] << ", " << mask1f[2] << ", " << mask1f[3] << "\n";
 
 	union { int first; float second; };
 	first = 2;
@@ -29,6 +29,6 @@ int main()
 	d4 = _mm_mul_ps(d4, e4);
 	std::cout << "d4 times e4 " << d[0] << ", " << d[1] << ", " << d[2] << ", " << d[3] << "\n";
 
-	__m128 c4 = _mm_min_ps(e4, d4);
-	std::cout << "min value e4 and d4 " << c4[0] << ", " << c4[1] << ", " << c4[2] << ", " << c4[3] << "\n";
+	/*__m128 c4 = _mm_min_ps(e4, d4);
+	std::cout << "min value e4 and d4 " << c4[0] << ", " << c4[1] << ", " << c4[2] << ", " << c4[3] << "\n";*/
 }
